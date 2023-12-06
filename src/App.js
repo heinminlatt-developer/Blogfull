80af771db35b5ee23d79df0095845c4e7e065bf5
HeinMinLatt1235 <123471377+HeinMinLatt1235@users.noreply.github.com>
2023-12-06 14:21:21 +0630

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+
+import './App.css';
+import { Table } from 'react-bootstrap';
+import headerImg from './assets/img/headerImg.png'
+import Topbar from './layouts/Topbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar/>
+      <img src={headerImg} />
+      <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+
+    </div>
+  );
+}
+
+export default App;
